52d35b6a730755e46eacd31dcde428e1e9cfae5a
Wiktor Mazur <wiktormazur1@gmail.com>
2017-01-25 20:49:19 +0100

    Since php is used almost everywhere php server included

---
+++ server.php
@@ -0,0 +1,22 @@
+<?php
+$scriptInvokedFromCli =
+    isset($_SERVER['argv'][0]) && $_SERVER['argv'][0] === 'server.php';
+if($scriptInvokedFromCli) {
+    $port = getenv('PORT');
+    if (empty($port)) {
+        $port = "3000";
+    }
+    echo 'starting server on port '. $port . PHP_EOL;
+    exec('php -S localhost:'. $port . ' -t dist server.php');
+} else {
+    return routeRequest();
+}
+function routeRequest()
+{
+    $uri = $_SERVER['REQUEST_URI'];
+    if ($uri == '/') {
+        echo file_get_contents('./dist/index.html');
+    } else {
+        return false;
+    }
+}
